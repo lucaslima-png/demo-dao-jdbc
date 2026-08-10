abcfc0209f5b3c6ad54a30b3fb75259799f98180
Lucas Lima <lucaslima13861@gamil.com>
2026-08-10 15:53:35 -0300

    novo commit

---
+++ src/application/Program.java
@@ -0,0 +1,13 @@
+package application;
+
+import model.entities.Department;
+
+public class Program {
+
+    static void main(String[] args) {
+
+        Department obj = new Department(1,"Books");
+        System.out.println(obj);
+
+    }
+}
