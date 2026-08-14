08efd8e46f22e36fe77f0d5ddb0daafc31509502
Lucas Lima <lucaslima13861@gamil.com>
2026-08-14 17:04:30 -0300

    delete implementation

--- src/db/DB.java
+++ src/db/DB.java
@@ -2,11 +2,7 @@ package db;
 
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.Properties;
 
 public class DB {
@@ -67,4 +63,6 @@ public class DB {
 			}
 		}
 	}
+
+
 }
